41d54b74b6ae1fff4badefb299868ffffa3629c1
SHOK <1723533633@qq.com>
2024-04-10 02:13:43 +0800

    优化首页

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,7 +1,15 @@
 <template>
-  <van-cell center title="心动模式">
+  <van-notice-bar color="#1989fa" background="#ecf9ff" left-icon="like-o">
+    即今江海一归客，他日云霄万里人。
+  </van-notice-bar>
+  <van-swipe class="my-swipe" :autoplay="3000" lazy-render>
+    <van-swipe-item v-for="image in images" :key="image">
+      <img :src="image" />
+    </van-swipe-item>
+  </van-swipe>
+  <van-cell center title="精准匹配">
     <template #right-icon>
-      <van-switch v-model="isMatchMode" />
+      <van-switch active-color="#ee0a24" v-model="isMatchMode" />
     </template>
   </van-cell>
   <user-card-list :user-list="userList" :loading="loading"/>
@@ -14,12 +22,22 @@ import myAxios from "../plugins/myAxios";
 import UserCardList from "../components/UserCardList.vue";
 import {showFailToast} from "vant";
 import {UserType} from "../models/user";
+import flower from "../assets/flower.png"
+import summer from "../assets/summer.png"
+import autumn from "../assets/autumn.png"
+import spring from "../assets/spring.png"
 
 const userList = ref([]);//存放用户列表
 
 const isMatchMode = ref<boolean>(false);
 const loading = ref(true);
 
+const images = [
+    flower,
+    summer,
+    autumn,
+    spring,
+];
 /**
  * 加载数据
  */
@@ -76,5 +94,11 @@ watchEffect(() => {
 </script>
 
 <style scoped>
+.my-swipe{
+  height: 180px;
+}
+.my-swipe img{
+  max-width: 100%;
+}
 
 </style>
